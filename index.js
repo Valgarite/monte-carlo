8e202f259869f74cdddc610c7631f1e43a6f3ed1
Carlos Gutiérrez <93498358+Valgarite@users.noreply.github.com>
2023-10-07 00:50:28 -0400

    plano cartesiano

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ const [graphCanvas, slider, numberInput] = inputs.map(id => { return document.ge
 
 const canvasSize = graphCanvas.width
 graphCanvas.height = canvasSize
-const squareSize = canvasSize / 8
-const [offsetX, offsetY] = [squareSize * 0, squareSize * 0]
+const squareSize = ((canvasSize / 32))
+const [offsetX, offsetY] = [squareSize * 2, squareSize * 12] //reemplazar el 0 por un input
+const [begin, finish] = [0, canvasSize]
 const ctx = graphCanvas.getContext("2d")
 console.log(graphCanvas)
 
 drawSim()
 
 function drawSim() {
-    const halfPx = canvasSize / 2
+    const halfPx = { x: Math.round(canvasSize / 2 + offsetY), y: Math.round(canvasSize / 2 - offsetX) }
+    console.log(offsetX, offsetY)
+    console.log(halfPx)
     for (let currentPx = 0; currentPx <= canvasSize; currentPx += squareSize) {
-        ctx.beginPath()
-        const drawingX = { y: currentPx + offsetY, begin: 0 + offsetX, finish: canvasSize + offsetX }
-        const drawingY = { x: currentPx + offsetX, begin: 0 + offsetY, finish: canvasSize + offsetY }
-        ctx.moveTo(drawingX.begin, drawingX.y)
-        ctx.lineTo(drawingX.finish, drawingX.y)
-        ctx.moveTo(drawingY.x, drawingY.begin)
-        ctx.lineTo(drawingY.x, drawingX.finish)
-        if (halfPx == currentPx) {
-            ctx.strokeStyle = "cyan"
-        } else {
-            ctx.strokeStyle = "orange"
-        }
-        ctx.stroke()
+        drawAxis(currentPx, halfPx)
     }
 }
 
-drawFunction(2, 3, 50)
+drawFunction(2, 3, 100)
 function drawFunction(start, end, length) {
     const xList = Array.from({ length }, (x, i) => {
-        return (end)
+        const unit = start + (end - start) / length * i
+        return unit
     })
     console.log(xList)
+}
+
+function drawAxis(currentPx, halfPx) {
+    const check = Math.round(currentPx)
+    ctx.beginPath()
+    ctx.moveTo(begin, currentPx)
+    ctx.lineTo(finish, currentPx)
+    if (halfPx.x == check) { ctx.strokeStyle = "cyan" } else { ctx.strokeStyle = "orange" }
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.moveTo(currentPx, begin)
+    ctx.lineTo(currentPx, finish)
+    if (halfPx.y == check) { ctx.strokeStyle = "cyan" } else { ctx.strokeStyle = "orange" }
+    ctx.stroke()
 }

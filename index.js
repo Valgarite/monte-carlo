ade1c49a4af4d4a7e6a303cce6d6e4967fb2d8ae
Carlos Gutiérrez <93498358+Valgarite@users.noreply.github.com>
2023-10-11 16:11:33 -0400

    Ajustar ejes

--- index.js
+++ index.js
@@ -1,27 +1,72 @@
 const inputs = ["graph", "sim-slider", "sim-quantity"]
 const [graphCanvas, slider, numberInput] = inputs.map(id => { return document.getElementById(id) })
-
 const canvasSize = graphCanvas.width
 graphCanvas.height = canvasSize
-const squareSize = ((canvasSize / 32))
-const [offsetX, offsetY] = [squareSize * 2, squareSize * 12] //reemplazar el 0 por un input
 const [begin, finish] = [0, canvasSize]
 const ctx = graphCanvas.getContext("2d")
-console.log(graphCanvas)
-
-drawSim()
+ctx.font = "16px Arial";
+ctx.fillStyle = "#fff"
+//Estos de acá abajo tienen valores ajustables.
+const tov = createTableOfValues(2, 3, 100)
+const separation = { x: 16, y: 16 }
+const squareSize = { x: canvasSize / separation.x, y: canvasSize / separation.y }
+const [offsetX, offsetY] = [squareSize.x * -4, squareSize.y * 7]
+const halfPx = { x: Math.round(canvasSize / 2 + offsetX), y: Math.round(canvasSize / 2 + offsetY) }
+const scale = { x: 2 / 1, y: 1 / 2 }
+// drawSim()
 
 function drawSim() {
-    const halfPx = { x: Math.round(canvasSize / 2 + offsetY), y: Math.round(canvasSize / 2 - offsetX) }
-    console.log(offsetX, offsetY)
-    console.log(halfPx)
-    for (let currentPx = 0; currentPx <= canvasSize; currentPx += squareSize) {
-        drawAxis(currentPx, halfPx)
+    for (let currentPx = 0; currentPx <= canvasSize; currentPx += squareSize.x) {
+        drawAxis(currentPx, halfPx, "y", scale)
     }
+    for (let currentPx = 0; currentPx <= canvasSize; currentPx += squareSize.y) {
+        drawAxis(currentPx, halfPx, "x", scale)
+    }
+    // console.log(tov)
+    drawFunction()
+    ctx.beginPath()
+    tov.xList.forEach((xValue, i)=>{
+        ctx.fillRect(xValue, )
+    })
 }
-
-console.log(crearTablaDeValores(2, 3, 100))
-function crearTablaDeValores(start, end, length) {
+function drawAxis(currentPx, halfPx, dir, scale) {
+    const check = Math.fround(currentPx)
+    const axisOffset = { x: 0 * squareSize.x, y: 0 * squareSize.y }
+    ctx.beginPath()
+    ctx.lineWidth = 3
+    if (dir == "x") {
+        ctx.moveTo(begin, currentPx)
+        ctx.lineTo(finish, currentPx)
+        const text = ((currentPx - halfPx.x) / separation.x / 2) / scale.x
+        if (text) {
+            ctx.fillText(text, currentPx, halfPx.y + axisOffset.y);
+        }
+        if (halfPx.y + axisOffset.y == check) { ctx.strokeStyle = "#00ff00" } else { ctx.strokeStyle = "#ff00ff" }
+    } else if (dir == "y") {
+        ctx.moveTo(currentPx, begin)
+        ctx.lineTo(currentPx, finish)
+        const text = -((currentPx - halfPx.y) / separation.y / 2) / scale.y
+        if (text) {
+            ctx.fillText(text, halfPx.x + axisOffset.x, currentPx);
+        }
+        if (halfPx.x + axisOffset.x == check) { ctx.strokeStyle = "#00ff00" } else { ctx.strokeStyle = "#ff00ff" }
+    }
+    ctx.stroke()
+}
+function drawFunction() {
+    ctx.beginPath()
+    ctx.moveTo((tov.xList[0] * squareSize * scale.x + halfPx.x), (tov.yList[0] * - squareSize * scale.x + halfPx.y))
+    tov.xList.forEach((xValue, i) => {
+        const x = xValue * squareSize.x * scale.x + halfPx.x
+        const y = tov.yList[i] * -squareSize.y * scale.y + halfPx.y
+        // console.log(x, y)
+        ctx.lineTo(x, y)
+        console.log(xValue)
+    });
+    ctx.strokeStyle = "#FFFF00"
+    ctx.stroke()
+}
+function createTableOfValues(start, end, length) {
     const xList = Array.from({ length }, (x, i) => {
         const unit = start + (end - start) / length * i
         return rounder(unit, 100)
@@ -30,22 +75,9 @@ function crearTablaDeValores(start, end, length) {
     const yList = xList.map((x) => { return rounder((3 * (x ** 2)), 100) })
     return { xList, yList }
 }
-
-function drawAxis(currentPx, halfPx) {
-    const check = Math.fround(currentPx)
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.moveTo(begin, currentPx)
-    ctx.lineTo(finish, currentPx)
-    if (halfPx.x == check) { ctx.strokeStyle = "cyan" } else { ctx.strokeStyle = "orange" }
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(currentPx, begin)
-    ctx.lineTo(currentPx, finish)
-    if (halfPx.y == check) { ctx.strokeStyle = "cyan" } else { ctx.strokeStyle = "orange" }
-    ctx.stroke()
-}
-
 function rounder(number, qtyToRound) {
     return Math.round(number * qtyToRound) / qtyToRound
+}
+function valueUpdate() {
+    
 }
